d763086840b7cab2cc436daa62d94a1b8b1affea
Luis Chavez <luischavezcald@gmail.com>
2023-08-17 12:36:54 -0400

    complete library config

--- projects/ngx-minimalist-ui/src/public-api.ts
+++ projects/ngx-minimalist-ui/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ngx-minimalist-ui
  */
-
-export * from './lib/ngx-minimalist-ui.service';
-export * from './lib/ngx-minimalist-ui.component';
-export * from './lib/ngx-minimalist-ui.module';
